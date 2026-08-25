d94efa468077215ff612fc2e0e09cb9c51c89a90
Pranto Ghosh <prantoas8002@gmail.com>
2026-08-25 21:53:57 +0600

    primaary setup of server

--- Student Management API/server.js
+++ Student Management API/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const router = require('./route');
+
 
 const app = express();
 
 
-
+app.use(express.json());
 app.use(cors());
+
+app.use("/api", require('./route'));
+
+
 
 app.listen(4000, () => {
     console.log("server is runnnig at port 4000 ...");
